db943fd3b1f5943850f9048ca4fae01a53e31fea
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-19 11:15:52 -0500

    Delete 'src/index.css'. Update title in 'index.html'. Import 'App.css' in 'App.jsx'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home"
-import Edit from "./pages/Edit"
-import FourOFour from "./pages/FourOFour"
-import Index from "./pages/Index"
-import New from "./pages/New"
-import Show from "./pages/Show"
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import FourOFour from './pages/FourOFour';
+import Index from './pages/Index';
+import New from './pages/New';
+import Show from './pages/Show';
 
 function App() {
-   
+	return (
+		<div>
+			<Router>
+				<NavBar />
+				<main>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/hotwheels' element={<Index />} />
+						<Route path='/hotwheels/new' element={<New />} />
+						<Route path='/hotwheels/:id' element={<Show />} />
+						<Route path='/hotwheels/:id/edit' element={<Edit />} />
+						<Route path='*' element={<FourOFour />} />
+					</Routes>
+				</main>
+			</Router>
+		</div>
+	);
+}
 
-  return (
-    <div>
-      <Router>
-        <NavBar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/hotwheels" element={<Index />} />
-            <Route path="/hotwheels/new" element={<New />} />
-            <Route path="/hotwheels/:id" element={<Show />} />
-            <Route path="/hotwheels/:id/edit" element={<Edit />} />
-            <Route path="*" element={<FourOFour />} />
-          </Routes>
-        </main>
-
-      </Router>
-    </div>
-
-  )
-};
-
-export default App
+export default App;
